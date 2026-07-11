45fc33bb0a20ad68c382c0bf908b5e083accb388
BhushanJagtap45 <bhushanjagtap057@gmail.com>
2026-07-11 11:11:19 +0530

    Update WiFi credentials in final.ino

--- Arduino_Code/final.ino
+++ Arduino_Code/final.ino
@@ -1,8 +1,8 @@
 #include <WiFi.h>
 #include <ESP32Servo.h>
 
-const char* ssid = "Jagtap WiFi";
-const char* password = "Bhushan@4321";   
+const char* ssid = "WiFi_Name";
+const char* password = "Wifi_Password";   
 
 Servo feederServo;
 int feedAngle = 60;
